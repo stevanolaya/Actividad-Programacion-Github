b96db193c567ffc9a369ef68d602487d98d98893
Etean01 <stevanolaya@gmail.com>
2025-04-07 20:32:14 -0500

    subiendo las correcciones pedidas y eliminando la carpeta que se creo con la dependencia de junit que se instanciaron al crear el proyecto

--- src/main/java/co/edu/etitc/actividad/programacion/github/greeting.java
+++ src/main/java/co/edu/etitc/actividad/programacion/github/greeting.java
@@ -3,7 +3,7 @@ package co.edu.etitc.actividad.programacion.github;
 import org.springframework.stereotype.Component;
 
 @Component
-public class greeting {    
+public class Greeting{    
     public void greet() {
         System.out.println("Hola mundo desde Spring!");
     }
